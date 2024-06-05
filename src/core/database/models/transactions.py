b18417205582a6fac5dc95a975d5d27cb50b86f8
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-05 19:06:53 +0300

    Added datetime fields to transactions table

--- src/core/database/models/transactions.py
+++ src/core/database/models/transactions.py
@@ -1,4 +1,6 @@
-from sqlalchemy import String, DECIMAL
+from datetime import datetime
+
+from sqlalchemy import String, DECIMAL, func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.core.database.db_settings.base import Base
@@ -10,6 +12,12 @@ class Transaction(UserRelationMixin, Base):
 
     type: Mapped[str] = mapped_column(String(100), unique=True, index=True, nullable=False)
     amount: Mapped[float] = mapped_column(DECIMAL(precision=10, scale=2), default=0.00)
+    created_at: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow, server_default=func.now()
+    )
+    updated_at: Mapped[datetime] = mapped_column(
+        default=datetime.utcnow, server_default=func.now()
+    )
 
     def __repr__(self):
         return self.type
