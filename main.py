1750081d4b8391848ab1f2859bc119b963fa5273
Viktor Beniukh <superbigbenjamin@gmail.com>
2024-06-05 16:49:23 +0300

    Added the project config and logging

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import uvicorn
+import logging
 
 from fastapi import FastAPI
 from fastapi_pagination import add_pagination
 from fastapi_pagination.utils import disable_installed_extensions_check
 
+from src.core.conf.logging_config import setup_logging
+
+setup_logging()
+
+logger = logging.getLogger(__name__)
 
 disable_installed_extensions_check()
 
@@ -21,6 +27,7 @@ def read_root():
 
     :return: dict: health status
     """
+    logger.info("User accessed the healthcheck page")
     return {"message": "Welcome to FastAPI project"}
 
 
